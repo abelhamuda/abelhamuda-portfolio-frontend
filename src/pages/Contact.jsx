a33b411399db1127bc7624d07db0d9c0dc20dc98
Yozabel Hamuda <yozabelhamuda@gmail.com>
2025-11-11 23:41:30 +0700

    update contact section

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -43,7 +43,7 @@ const Contact = () => {
     },
     {
       name: 'Twitter',
-      url: 'https://twitter.com/abelhamuda',
+      url: 'https://twitter.com/ablhamuda',
       icon: 'M23 3a10.9 10.9 0 0 1-3.14 1.53 4.48 4.48 0 0 0-7.86 3v1A10.66 10.66 0 0 1 3 4s-4 9 5 13a11.64 11.64 0 0 1-7 2c9 5 20 0 20-11.5a4.5 4.5 0 0 0-.08-.83A7.72 7.72 0 0 0 23 3z'
     },
     {
@@ -204,7 +204,7 @@ const Contact = () => {
               <div className="space-y-4">
                 {[
                   { icon: 'M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z', 
-                    label: 'Email', value: 'abel@hamuda.com' },
+                    label: 'Email', value: 'yozabelhamuda@gmail.com' },
                   { icon: 'M3 5a2 2 0 012-2h3.28a1 1 0 01.948.684l1.498 4.493a1 1 0 01-.502 1.21l-2.257 1.13a11.042 11.042 0 005.516 5.516l1.13-2.257a1 1 0 011.21-.502l4.493 1.498a1 1 0 01.684.949V19a2 2 0 01-2 2h-1C9.716 21 3 14.284 3 6V5z', 
                     label: 'Phone', value: '+1 (555) 123-4567' },
                   { icon: 'M17.657 16.657L13.414 20.9a1.998 1.998 0 01-2.827 0l-4.244-4.243a8 8 0 1111.314 0z M15 11a3 3 0 11-6 0 3 3 0 016 0z', 
